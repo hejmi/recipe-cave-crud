939cc43feb3493a2e5c572e98a2a8de405fa9e00
MichMatch <michael@graphicdesire.se>
2021-09-07 12:56:12 +0200

    server start fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express, { json } from "express";
+import express from "express";
 import db from "./config/db.js";
 import cors from "cors";
 
 const app = express();
 const PORT = 3002;
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 // Route to get all posts
 app.get("/api/get", (req, res) => {
-	db("SELECT * FROM recipe", (err, result) => {
+	db.query("SELECT * FROM recipe", (err, result) => {
 		if (err) {
 			console.log(err);
 		}
@@ -20,10 +20,14 @@ app.get("/api/get", (req, res) => {
 // Route to get one post
 app.get("/api/getFromId/:id", (req, res) => {
 	const id = req.params.id;
-	db("SELECT * FROM recipe WHERE id = ?", id, (err, result) => {
+	db.query("SELECT * FROM recipe WHERE id = ?", id, (err, result) => {
 		if (err) {
 			console.log(err);
 		}
 		res.send(result);
 	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`);
 });
